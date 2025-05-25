1b9dce0dff1d4997a38704061bfefd81c2ba8bdb
JinyuanZhou <jinyuanleo@gmail.com>
2025-05-25 13:26:33 +0800

    feat(ModernTableView): implement row insertion with validation and logging; add row removal functionality

---
+++ tkScrollableWindow.py
@@ -0,0 +1,65 @@
+import ttkbootstrap as ttk
+import tkinter as tk
+from tkinter import messagebox
+from ttkbootstrap.constants import *
+
+
+class tkScrollableWindow(ttk.Window):
+    def __init__(
+        self,
+        title="ttkbootstrap",
+        themename="darkly",
+        iconphoto="",
+        size=None,
+        position=None,
+        minsize=None,
+        maxsize=None,
+        resizable=None,
+        hdpi=True,
+        scaling=None,
+        transient=None,
+        overrideredirect=False,
+        alpha=1,
+        **kwargs,
+    ):
+        super().__init__(
+            title,
+            themename,
+            iconphoto,
+            size,
+            position,
+            minsize,
+            maxsize,
+            resizable,
+            hdpi,
+            scaling,
+            transient,
+            overrideredirect,
+            alpha,
+            **kwargs,
+        )
+        self.canvas = tk.Canvas(self)
+        self.scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.scrollable_frame = ttk.Frame(self.canvas)
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+        self.canvas.pack(side="left", fill="both", expand=True, ipadx=10, ipady=10, padx=10, pady=10)
+        self.scrollbar.pack(side="right", fill="y")
+
+        style = ttk.Style()
+        style.configure(".", font="Helvetica")
+
+    def _on_mousewheel(self, event):
+        if self.canvas.winfo_height() < self.canvas.bbox("all")[3]:
+            direction = -1 if event.delta > 0 else 1
+            self.canvas.yview_scroll(direction, "units")
+
+    def _update_scrollregion(self):
+        self.scrollable_frame.update_idletasks()
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+
+if __name__ == "__main__":
+    root = tkScrollableWindow(themename="darkly")
+    root.mainloop()
---
+++ tkTableView.py
@@ -0,0 +1,59 @@
+import ttkbootstrap as ttk
+import tkinter as tk
+from tkinter import messagebox
+from ttkbootstrap.tableview import TableRow, Tableview
+from typing import Callable, Any
+from loguru import logger
+
+
+class ModernTableView(Tableview):
+    def __init__(
+        self,
+        master=None,
+        bootstyle=...,
+        coldata=...,
+        rowdata=...,
+        paginated=False,
+        searchable=False,
+        yscrollbar=False,
+        autofit=False,
+        autoalign=True,
+        stripecolor=None,
+        pagesize=10,
+        height=10,
+        delimiter=",",
+    ):
+        super().__init__(
+            master,
+            bootstyle,
+            coldata,
+            rowdata,
+            paginated,
+            searchable,
+            yscrollbar,
+            autofit,
+            autoalign,
+            stripecolor,
+            pagesize,
+            height,
+            delimiter,
+        )
+
+    def insert_rows(self, index, rowdata, validator: Callable[..., bool] = lambda x: True) -> None:
+        if validator(rowdata):
+            logger.info(f"Inserted row data: {rowdata}")
+            return super().insert_rows(index, rowdata)
+        else:
+            logger.error(f"Invalid row data: {rowdata}")
+
+    def insert_row(self, index: str = "end", values=[], validator: Callable[..., bool] = lambda x: True) -> TableRow:
+        if validator(values):
+            return super().insert_row(index, values)
+        else:
+            logger.error(f"Invalid row data: {values}")
+
+    def remove_selected_rows(self):
+        selected_iids = self.view.selection()
+        if selected_iids:
+            # 删除选中的行
+            self.delete_rows(iids=selected_iids)
